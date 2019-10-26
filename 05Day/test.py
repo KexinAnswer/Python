2d3a2eb93cfee7fe1d354cf1d46df2893f00ba6f
KexinAnswer <925433202@qq.com>
2019-10-26 23:12:56 +0800

    斐波那契数列

--- 05Day/test.py
+++ 05Day/test.py
@@ -3,7 +3,7 @@
 # num = int(input("请输入一个数->"))
 # res = num
 # sum = 0
-# 
+#  
 # for i in range(0, 3):
 #     num1 = num % 10
 #     num = num // 10 
@@ -34,17 +34,62 @@
 #         k = 100 - i - j
 #         if 5 * i + 3 * j + k / 3 == 100:
 #             print('公鸡 %d 只， 母鸡 %d 只， 小鸡 %d 只' % (i, j, k))
+
+
+# from random import randint
 # 
+# money = 1000
+# print(type(money))
+# while money > 0:
+#     print('你的总资产为:', money)
+#     needs_go_on = False
+#     while True:
+#         debt = int(input('请下注:'))
+#         if 0 < debt <= money:
+#             break
+#     first = randint(1, 6) + randint(1, 6)
+#     print('玩家摇出了%d点' % first)
+#     if first == 7 or first == 11:
+#         print('玩家胜!')
+#         money += debt
+#     elif first == 2 or first == 3 or first == 12:
+#         print('庄家胜!')
+#         money -= debt
+#     else:
+#         needs_go_on = True
+#     while needs_go_on:
+#         needs_go_on = False
+#         current = randint(1, 6) + randint(1, 6)
+#         print('玩家摇出了%d点' % current)
+#         if current == 7:
+#             print('庄家胜')
+#             money -= debt
+#         elif current == first:
+#             print('玩家胜')
+#             money += debt
+#         else:
+#             needs_go_on = True
+# print('你破产了，游戏结束！')
 
 
-from random import randint
+# 斐波那契数列
 
-money = 100
-while money > 0:
-    print('你的总资产为:', money)
-    needs_go_on = false
-    while True:
-        debt = int(input('请下注:'))
-        if 0 < debt <= money:
-            break
-    first = randint(1, 6) + randint(1, 6)
+def Feb(num):
+    if num == 1 or num == 2:
+        return 1
+    a = 1
+    b = 1
+    c = 2
+    i = 3
+    while i <= num:
+        c = a + b
+        a = b
+        b = c
+        i = i + 1
+    print(c)
+    return c
+
+num = int(input('请输入一个正整数'))
+print(num)
+res = Feb(num)
+print(res)
